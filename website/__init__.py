17078cbb4c961c8ef616fdb544062fc6508f8038
Ayushkabra09 <ayush.kabra09@gmail.com>
2023-07-08 12:07:19 +0530

    pushing ogin flow

---
+++ website/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "asset_management"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "secret_key"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://Ayush:hitkar123@localhost/{DB_NAME}'
+    db.init_app(app)
+    
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    # create_database(app)
+
+    return app
+
---
+++ website/models.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from . import db
+from flask_login import UserMixin
+
+class AssetCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # Add more fields as needed
+
+    # def __repr__(self):
+    #     return self.name
+
+class Asset(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('asset_category.id'), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    asset_name = db.Column(db.String(100), nullable=False)
+    serial_number = db.Column(db.String(50), unique=True, nullable=False)
+    purchase_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    purchase_cost = db.Column(db.Float, nullable=False)
+    current_value = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    assigned_to = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    description = db.Column(db.Text, nullable=True)
+    # Add more fields as needed
+
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # def __repr__(self):
+    #     return self.asset_name
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
